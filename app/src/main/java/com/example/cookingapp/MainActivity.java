1f3b35f0f027a1ccdff8ee2eeb5f15a18a8cc86f
aesen <s_esenalieva23@stud.hwr-berlin>
2025-07-08 23:41:39 +0200

    App starten mit entweder Home/Login

--- app/src/main/java/com/example/cookingapp/MainActivity.java
+++ app/src/main/java/com/example/cookingapp/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.cookingapp;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,8 +12,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //mini speicher für user um zu gucken ob dieseer eingeloggt ist
+        SharedPreferences pr = getSharedPreferences("user", MODE_PRIVATE);
+        boolean loggedIn = pr.getBoolean("loggedIn", false);
 
-
+        if(loggedIn){
+            startActivity(new Intent(this, HomeActivity.class));
+        } else{
+            startActivity(new Intent(this, LoginActivity.class));
+        }
+        finish();
         }
 
     }
