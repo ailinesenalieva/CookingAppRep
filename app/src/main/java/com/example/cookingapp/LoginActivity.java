7aa97c8965617d641d2b16f299a27c731f9ad0b6
aesen <s_esenalieva23@stud.hwr-berlin>
2025-07-09 09:53:42 +0200

    Login mit button und text eingefügt

--- app/src/main/java/com/example/cookingapp/LoginActivity.java
+++ app/src/main/java/com/example/cookingapp/LoginActivity.java
@@ -1,12 +1,17 @@
 package com.example.cookingapp;
 
 import android.os.Bundle;
+import android.annotation.SuppressLint;
 import androidx.appcompat.app.AppCompatActivity;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Button LoginButton = findViewById(R.id.buttonlog);
+        LoginButton.setOnClickListener();
     }
 }
