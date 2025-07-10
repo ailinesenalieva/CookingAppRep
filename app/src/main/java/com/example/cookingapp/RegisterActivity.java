50a8691680870ff835de63750206a081fba9872f
aesen <s_esenalieva23@stud.hwr-berlin>
2025-07-10 17:17:57 +0200

    Daten an Firebase senden

--- app/src/main/java/com/example/cookingapp/RegisterActivity.java
+++ app/src/main/java/com/example/cookingapp/RegisterActivity.java
@@ -7,28 +7,39 @@ import android.widget.Button;
 import android.view.View;
 import java.lang.Object;
 import android.content.Intent;
+import android.widget.EditText;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import androidx.appcompat.app.AppCompatActivity;
 
 
 
 public class RegisterActivity extends AppCompatActivity {
+    //aus register xml importieren
+    private EditText inputname;
+    private EditText inputemail;
+    private EditText inputpassword;
+    private Button buttonRegister;
+    private DatabaseReference rootDatabase;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        inputname = findViewById(R.id.inputname);
+        inputemail=findViewById(R.id.inputemail);
+        inputpassword=findViewById(R.id.inputpassword);
+        buttonRegister=findViewById(R.id.buttonRegister);
+        rootDatabase = FirebaseDatabase.getInstance().getReference().child("user");
 
-        final Button RegisterButton = findViewById(R.id.buttonreg);
-
-
-
-
-        RegisterButton.setOnClickListener(new View.OnClickListener() {
-            @SuppressLint("SetTextI18n")
+        buttonRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-
             }
         });
-    }
-}
+
+
+
+
+            }
+        }
