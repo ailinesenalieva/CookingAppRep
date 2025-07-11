4d8bda28ff2a304b45799391450dd74bb88d7f74
aesen <s_esenalieva23@stud.hwr-berlin>
2025-07-11 21:08:00 +0200

    logik zum geändert ohne zu gucken ob user eingeloggt ist, user entscheidet selbst ob register oder login button gedrückt wird

--- app/src/main/java/com/example/cookingapp/MainActivity.java
+++ app/src/main/java/com/example/cookingapp/MainActivity.java
@@ -1,28 +1,35 @@
 package com.example.cookingapp;
 
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-
-
 public class MainActivity extends AppCompatActivity {
+
+    private Button buttonlog;
+    private Button buttonreg;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //mini speicher für user um zu gucken ob dieseer eingeloggt ist
-        SharedPreferences pr = getSharedPreferences("user", MODE_PRIVATE);
-        boolean loggedIn = pr.getBoolean("loggedIn", false);
 
-        if(loggedIn){
-            startActivity(new Intent(this, HomeActivity.class));
-        } else{
-            startActivity(new Intent(this, LoginActivity.class));
-        }
-        finish();
-        }
+        buttonlog = findViewById(R.id.buttonlog);
+        buttonreg = findViewById(R.id.buttonreg);
 
+        buttonlog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, LoginActivity.class));
+            }});
+        buttonreg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, RegisterActivity.class));
+            }
+        });
     }
-
+}
