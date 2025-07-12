ed4306d8524da5122de3dba36134a3caf3f7cc35
aesen <s_esenalieva23@stud.hwr-berlin>
2025-07-12 19:14:05 +0200

    home buttons search und profil angelegt

--- app/src/main/java/com/example/cookingapp/HomeActivity.java
+++ app/src/main/java/com/example/cookingapp/HomeActivity.java
@@ -1,12 +1,38 @@
 package com.example.cookingapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class HomeActivity extends AppCompatActivity {
+
+    private Button buttonhomesearch;
+    private Button buttonprofile;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        buttonhomesearch = findViewById(R.id.buttonhomesearch);
+        buttonprofile= findViewById(R.id.buttonprofile);
+
+        //weiterführung zu Profile des Users
+        buttonprofile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(HomeActivity.this, ProfileActivity.class));
+            }
+        });
+        //weiterführung zur search actiivty
+        buttonhomesearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
