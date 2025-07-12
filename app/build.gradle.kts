59aae5f4e91bc0c34270bdcfd5f310fbac8d5d10
aesen <s_esenalieva23@stud.hwr-berlin>
2025-07-12 13:44:05 +0200

    implementation eingefügt

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,9 +38,14 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation(libs.firebase.database)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
-    implementation(platform("com.google.firebase:firebase-bom:33.16.0"))
-    implementation("com.google.firebase:firebase-analytics")
+    implementation(platform(libs.firebase.bom))
+    implementation(libs.firebase.analytics)
+    implementation("com.google.firebase:firebase-database:21.0.0")
+
+
+
 }
