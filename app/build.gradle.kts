fd268c6df3c584fac08314680be555d647e27c74
aesen <s_esenalieva23@stud.hwr-berlin>
2025-07-17 14:34:18 +0200

    picasso für die Darstellung der Bilder in results

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,6 +48,7 @@ dependencies {
 
     implementation("com.google.firebase:firebase-database:21.0.0")
     implementation("com.android.volley:volley:1.2.1")
+    implementation ("com.squareup.picasso:picasso:2.71828")
 
 
 }
