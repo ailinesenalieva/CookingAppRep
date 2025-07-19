5a48ffc57c6f0a8b0dde0eb0382000ba7711c806
aesen <s_esenalieva23@stud.hwr-berlin>
2025-07-19 17:16:27 +0200

    user klasse

--- app/src/main/java/com/example/cookingapp/User.java
+++ app/src/main/java/com/example/cookingapp/User.java
@@ -1,15 +1,26 @@
 package com.example.cookingapp;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class User {
+    public String userId;
     public String name;
     public String email;
     public String password;
+    private List<String> favorites;
+    private List<String> cookedRecipes;
+    private List<String> ownrecipes;
 
     public User() {}
 
-    public User(String name, String email, String password){
+    public User(String userId, String name, String email, String password){
+        this.userId = userId;;
         this.name = name;
         this.email=email;
         this.password = password;
+        this.favorites = new ArrayList<>();
+        this.cookedRecipes = new ArrayList<>();
+        this.ownrecipes = new ArrayList<>();
     }
 }
