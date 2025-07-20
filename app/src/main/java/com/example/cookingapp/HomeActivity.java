1c086ffa27ee38a99b21434e43cdbe75cb845e06
aesen <s_esenalieva23@stud.hwr-berlin>
2025-07-20 10:34:28 +0200

    logout button hinzugefügt

--- app/src/main/java/com/example/cookingapp/HomeActivity.java
+++ app/src/main/java/com/example/cookingapp/HomeActivity.java
@@ -4,13 +4,17 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class HomeActivity extends AppCompatActivity {
 
     private Button buttonhomesearch;
     private Button buttonprofile;
+    private Button homelogout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +23,7 @@ public class HomeActivity extends AppCompatActivity {
 
         buttonhomesearch = findViewById(R.id.buttonhomesearch);
         buttonprofile= findViewById(R.id.buttonprofile);
+        homelogout = findViewById(R.id.homelogout);
 
         //weiterführung zu Profile des Users
         buttonprofile.setOnClickListener(new View.OnClickListener() {
@@ -32,6 +37,16 @@ public class HomeActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
             startActivity(new Intent(HomeActivity.this, BrowseActivity.class));
+            }
+        });
+        homelogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                Intent intent = new Intent(HomeActivity.this, LoginActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK); // clear back stack
+                startActivity(intent);
+                Toast.makeText(HomeActivity.this, "You logged out successfully", Toast.LENGTH_SHORT).show();
             }
         });
     }
