2989f857828b10c2f68b98452ea590439bf77783
aesen <s_esenalieva23@stud.hwr-berlin>
2025-07-21 20:50:28 +0200

    kommentare hinzugefügt

--- app/src/main/java/com/example/cookingapp/MainActivity.java
+++ app/src/main/java/com/example/cookingapp/MainActivity.java
@@ -7,7 +7,7 @@ import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity { //Beginn Punkt der App
 
     private Button buttonlog;
     private Button buttonreg;
@@ -20,11 +20,13 @@ public class MainActivity extends AppCompatActivity {
         buttonlog = findViewById(R.id.buttonlog);
         buttonreg = findViewById(R.id.buttonreg);
 
+        //Login wenn user bereits registriert
         buttonlog.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(MainActivity.this, LoginActivity.class));
             }});
+        //Register wenn user noch nicht existiert
         buttonreg.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
