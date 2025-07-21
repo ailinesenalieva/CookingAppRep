078f1f83b4516b5d5803568d2dc6d7004f13e3f5
aesen <s_esenalieva23@stud.hwr-berlin>
2025-07-21 15:07:50 +0200

    kommentare hinzugefügt

--- app/src/main/java/com/example/cookingapp/ResultsActivity.java
+++ app/src/main/java/com/example/cookingapp/ResultsActivity.java
@@ -2,6 +2,7 @@ package com.example.cookingapp;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
@@ -9,7 +10,7 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.squareup.picasso.Picasso;
+import com.squareup.picasso.Picasso; // Image-Loading-Bibliothek für Android um Bild URLS einfach einfügen zu können
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -29,29 +30,43 @@ public class ResultsActivity extends AppCompatActivity {
         backhome = findViewById(R.id.backhome);
         backprofile = findViewById(R.id.backprofile);
 
+        //mealList aus Browse Activity die als Extra mitgegebne wurden
         ArrayList<HashMap<String, String>> mealList = (ArrayList<HashMap<String, String>>) getIntent().getSerializableExtra("results");
 
+        //wenn meallist inhalt besitzt, wird durch jedes Hashmap paar iteriert und zu resultcard methode angezeigt
         if (mealList != null && !mealList.isEmpty()) {
             for (HashMap<String, String> meal : mealList) {
                 String name = meal.get("name");
                 String imageUrl = meal.get("imageURL");
                 addResultCard(meal);
             }
-        } else {
+        } else { //wenn keine resultate für die suche ergeben
             TextView noResult = new TextView(this);
-            noResult.setText("Sorry, there are no recipes.");
+            noResult.setText(getString(R.string.norecipes));
             resultslayout.addView(noResult);
         }
 
-        backtobrowse.setOnClickListener(v -> {
-            startActivity(new Intent(ResultsActivity.this, BrowseActivity.class));
-            finish();
-        });
+        backtobrowse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(ResultsActivity.this, BrowseActivity.class));
+                finish();
+            }});
 
-        backhome.setOnClickListener(v -> startActivity(new Intent(ResultsActivity.this, HomeActivity.class)));
-        backprofile.setOnClickListener(v -> startActivity(new Intent(ResultsActivity.this, ProfileActivity.class)));
+        backhome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(ResultsActivity.this, HomeActivity.class));
+            }});
+        backprofile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(ResultsActivity.this, ProfileActivity.class));
+            }});
     }
 
+
+    //Rezepte aus meallist mit namen und bild werden horizontal aufgestellt
     private void addResultCard(HashMap<String, String> meal) {
         LinearLayout rowLayout = new LinearLayout(this);
         rowLayout.setOrientation(LinearLayout.HORIZONTAL);
@@ -61,10 +76,13 @@ public class ResultsActivity extends AppCompatActivity {
                 LinearLayout.LayoutParams.WRAP_CONTENT
         ));
 
-        LinearLayout card = createCard(meal);
+        LinearLayout card = createCard(meal); //einzelne rezepte (name + bild nebeneinander)
         rowLayout.addView(card);
-        resultslayout.addView(rowLayout);
+        resultslayout.addView(rowLayout); //zu gesamten view hinzufügen
     }
+
+
+
 
     private LinearLayout createCard(HashMap<String, String> meal) {
         LinearLayout card = new LinearLayout(this);
