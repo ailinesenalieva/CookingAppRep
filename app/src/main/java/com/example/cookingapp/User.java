ddcee5055a56756b9c50b97ccf967ff62a60b5f9
aesen <s_esenalieva23@stud.hwr-berlin>
2025-07-21 20:22:22 +0200

    user klasse mit getter und setter

--- app/src/main/java/com/example/cookingapp/User.java
+++ app/src/main/java/com/example/cookingapp/User.java
@@ -8,8 +8,7 @@ public class User {
     public String name;
     public String email;
     public String password;
-    private List<String> favorites;
-    private List<String> cookedRecipes;
+
     private List<String> ownrecipes;
 
     public User() {}
@@ -19,8 +18,53 @@ public class User {
         this.name = name;
         this.email=email;
         this.password = password;
-        this.favorites = new ArrayList<>();
-        this.cookedRecipes = new ArrayList<>();
         this.ownrecipes = new ArrayList<>();
     }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public List<String> getOwnrecipes() {
+        return ownrecipes;
+    }
+
+    public void setOwnrecipes(List<String> ownrecipes) {
+        this.ownrecipes = ownrecipes;
+    }
+
+    public void addOwnRecipe(String recipeId) {
+        if (this.ownrecipes == null) {
+            this.ownrecipes = new ArrayList<>();
+        }
+        this.ownrecipes.add(recipeId);
+    }
 }
+
